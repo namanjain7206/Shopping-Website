49223509bbd4bf36a1c17c52bd60fc8ad9e6ab6e
namanjain7206 <47470169+namanjain7206@users.noreply.github.com>
2020-07-22 19:18:56 +0530

    added a model

--- Ecart/Shop/models.py
+++ Ecart/Shop/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    prod_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=300)
+    publish_date= models.DateField()
+
+
