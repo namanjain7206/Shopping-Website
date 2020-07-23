805b8825ba2e646139b565d42eebca2ae688dff0
namanjain7206 <47470169+namanjain7206@users.noreply.github.com>
2020-07-23 15:04:47 +0530

    added basic.html

--- Ecart/Shop/views.py
+++ Ecart/Shop/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'Shop/index.html')
+
+def contact(request):
+    return HttpResponse(" contact ")
+def productview(request):
+    return HttpResponse(" productview ")
+def checkout(request):
+    return HttpResponse(" checkout ")
+def search(request):
+    return HttpResponse(" search ")
+def tracker(request):
+    return HttpResponse(" tracker ")
+def about(request):
+    return render(request,'Shop/about.html')
