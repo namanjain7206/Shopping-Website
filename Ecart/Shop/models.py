92e9805c94d3e54b18c35bcf10df8da51160f14c
namanjain7206 <47470169+namanjain7206@users.noreply.github.com>
2020-07-24 13:52:41 +0530

    added content to slides

--- Ecart/Shop/models.py
+++ Ecart/Shop/models.py
@@ -4,8 +4,13 @@ from django.db import models
 class Product(models.Model):
     prod_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    price = models.IntegerField()
     desc = models.CharField(max_length=300)
+    category = models.CharField(max_length=50, default='')
+    sub_category = models.CharField(max_length=50, default='')
+    price = models.IntegerField()
     publish_date= models.DateField()
+    images = models.ImageField(upload_to='Shop/images',default='')
 
+    def __str__(self):
+        return  self.product_name
 
--- Ecart/Shop/views.py
+++ Ecart/Shop/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,'Shop/index.html')
+    p=  Product.objects.all()
+    n= len(p)
+    nSlides = n//4 + ceil(n/4 - n//4)
+    print(nSlides)
+    params={'nSlides':nSlides,'range':range(1,nSlides),'product': p,'no_of_product': n }
+    return render(request,'Shop/index.html',params)
 
 def contact(request):
     return HttpResponse(" contact ")
